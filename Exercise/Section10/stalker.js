cc7a4755e63f1b9904265bea2621ddf5b69d3df4
robin-qu <hongbinqu9@gmail.com>
2019-07-29 15:31:09 -0700

    js basics

---
+++ Exercise/Section10/stalker.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What's your first name?");
+var lastName = prompt("What's your last name?");
+var age = prompt("How old are you?");
+
+console.log("Your name is " + firstName + " " + lastName + ", you are " + age + " years old");
