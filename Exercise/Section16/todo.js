d9772f84af6dc0626ad31f2011e1700ec421c147
robin-qu <hongbinqu9@gmail.com>
2019-08-01 17:24:21 -0700

    events demo

---
+++ Exercise/Section16/todo.js
@@ -0,0 +1,15 @@
+var LIs = document.querySelectorAll("li");
+
+for (var i = 0; i < LIs.length; i++) {
+    LIs[i].addEventListener("mouseover", function() {
+        this.classList.add("selected");
+    });
+
+    LIs[i].addEventListener("mouseout", function() {
+        this.classList.remove("selected");
+    })
+
+    LIs[i].addEventListener("click", function() {
+        this.classList.toggle("done");
+    })
+}
